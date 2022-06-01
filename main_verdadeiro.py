33913a51d3e7b665e469f05aee63f93bc4ebb3cb
Carlos Reis <carlosreis10.06@hotmail.com>
2022-05-31 21:42:57 -0300

    fix: gitignore images

---
+++ main_verdadeiro.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+
+while True:
+
+    option = input("Digite 1 para campainha e 2 para reconhecimento facial: ")
+
+    if option == "1":
+        print("a")
+    elif option == "2":
+        print ("Inicio do reconhecimento facial")
